2f27a6ba5fdc34201687ae549e794a7aca9b362c
Ivanov Dmitriy <usr345@gmail.com>
2026-09-25 17:56:02 +0300

    connectivity improved

--- Bet_le.thy
+++ Bet_le.thy
@@ -57,30 +57,64 @@ proof -
   have C1DA: "Bet C' D A" by (rule bet_sym [OF ADC_1])
   have DC1B1: "Bet D C' B'" by (rule CBA_ACD_BCD [OF C1DA ACB_1])
 
-  have BC1B1: "Bet B C' B'"
-  proof (cases "B = D")
+  show "Bet A C D \<or> Bet A D C"
+  proof (cases "D = C'")
 
-    assume B_eq_D: "B = D"
+    assume D_eq_C1: "D = C'"
 
-    have ABC1: "Bet A B C'" using B_eq_D ADC_1 by (rule ssubst)
-    have C1BA: "Bet C' B A" by (rule bet_sym [OF ABC1])
-    show "Bet B C' B'" by (rule CBA_ACD_BCD [OF C1BA ACB_1])
+    have C1C1_CD: "Congr C' C' C D" using D_eq_C1 DC1_CD by (rule subst)
+    have CD_C1C1: "Congr C D C' C'" by (rule congr_sym [OF C1C1_CD])
+    have C_eq_D: "C = D" by (rule congr_id [OF CD_C1C1])
+    have ACC: "Bet A C C" by (rule bet_right [of A C])
+    have ACD: "Bet A C D" using C_eq_D ACC by (rule subst)
+    show "Bet A C D \<or> Bet A D C" using ACD by (rule disjI1)
   next
 
-    assume B_neq_D: "B \<noteq> D"
+    assume D_neq_C1: "D \<noteq> C'"
 
-    thm outer_transitivity_between2 [of B D C' B']
-    show "Bet B C' B'" using BDC1 DC1B1  by (rule outer_transitivity_between2 [of B D C' B'])
-    (* BDC1 D C' B' *)
+    have BC1B1: "Bet B C' B'" using BDC1 DC1B1 D_neq_C1 by (rule outer_transitivity_between2 [of B D C' B'])
+
+    have CBA: "Bet C B A" by (rule bet_sym [OF ABC])
+    have BCD1: "Bet B C D'" by (rule CBA_ACD_BCD [OF CBA ACD_1])
+
+    show "Bet A C D \<or> Bet A D C"
+    proof (cases "C = D'")
+      assume C_eq_D1: "C = D'"
+
+      have D1D1_CD: "Congr D' D' C D" using C_eq_D1 CD1_CD by (rule subst)
+      have CD_D1D1: "Congr C D D' D'" by (rule congr_sym [OF D1D1_CD])
+
+      have C_eq_D: "C = D" using CD_D1D1 by (rule congr_id)
+      have ACC: "Bet A C C" by (rule bet_right [of A C])
+      have ACD: "Bet A C D" using C_eq_D ACC by (rule subst)
+      show "Bet A C D \<or> Bet A D C" using ACD by (rule disjI1)
+    next
+      assume C_neq_D1: "C \<noteq> D'"
+
+      have BCB2: "Bet B C B2" using BCD1 CD1B2 C_neq_D1 by (rule bet_outer_trans [of B C D' B2])
+      have B2CB: "Bet B2 C B" by (rule bet_sym [OF BCB2])
+      have BB1_B2B: "Congr B B' B2 B" using BC1B1 B2CB BC1_B2C CB1_CB by (rule congr_summa [of B C' B' B2 C B])
+
+      have ACB2: "Bet A C B2" by (rule ACD_ABC_ABD [OF AD1B2 ACD_1])
+      have ABB2: "Bet A B B2" by (rule ACD_ABC_ABD [OF ACB2 ABC])
+      have BB2_BB2: "Congr B B2 B B2" by (rule congr_refl [of B B2])
+
+      have ABC1: "Bet A B C'" by (rule ACD_ABC_ABD [OF ADC_1 ABD])
+      have ABB1: "Bet A B B'" by (rule ACD_ABC_ABD [OF ACB_1 ABC1])
+
+      have BB1_BB2: "Congr B B' B B2" by (rule congr_right_comm [OF BB1_B2B])
+      have B2_eq_B1: "B2 = B'" using A_neq_B ABB2 BB2_BB2 ABB1 BB1_BB2 
+        by (rule construction_uniqueness [of A B B2 B B2 B'])
+
+      have AD1B1: "Bet A D' B'" using B2_eq_B1 AD1B2 by (rule subst)
+      have B1D1C: "Bet B' D' C" using B2_eq_B1 B2D1C by (rule subst)
+      have BD_B1D1: "Congr B D B' D'" using B2_eq_B1 BD_B2D1 by (rule subst)
+    qed
+    
+    
   qed
-  thm congr_summa [of B C' B' B2 C B]
+  
 (*
-Bet B C' B' <- ?
-Bet B2 C B
-Congr B C' B2 C --- BC1_B2C
-Congr C' B' C B --- CB1_CB
-------------------
-Congr B B' B2 B
 
 A C' B'
 A D C'
@@ -90,4 +124,3 @@ A B D
 B D C'
 *)
 
-  have BC1_B2C: "B B' B2 B" by rule congr_summa
